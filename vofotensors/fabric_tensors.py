791e3b5f7b882869b62653a48aa4a8c3ef0ad875
JulianKarlBauer <juliankarlbauer@gmx.de>
2021-12-17 19:49:56 +0100

    Fix labels

--- vofotensors/fabric_tensors.py
+++ vofotensors/fabric_tensors.py
@@ -27,13 +27,13 @@ utils.map_nested(dictionary=N2s_parametric, transformation=utils.dev2_to_N2)
 
 N4s_pairs = {
     "planar": {
-        "alpha1_d_0_d_7": {
+        "alpha1_d1_d8": {
             "D2": deviators_2.dev2_planar_by_alpha1(),
-            "D4": deviators_4.dev4_planar_alpha1_d0_d7(),
+            "D4": deviators_4.dev4_planar_alpha1_d1_d8(),
         },
-        "la_0_d_0_d_7": {
-            "D2": deviators_2.dev2_planar_by_la_0(),
-            "D4": deviators_4.dev4_planar_la0_d0_d7(),
+        "la1_d1_d8": {
+            "D2": deviators_2.dev2_planar_by_la1(),
+            "D4": deviators_4.dev4_planar_la1_d1_d8(),
         },
     },
     "transv_isotropic": {
@@ -51,36 +51,34 @@ N4s_pairs = {
         },
     },
     "orthotropic": {
-        "a2_b2_a_b_c": {
-            "D2": deviators_2.dev2_by_a2_b2(),
+        "la1_l2_d1_d2_d3": {
+            "D2": deviators_2.dev2_by_la1_la2(),
             "D4": deviators_4.dev4_ortho_by_d1_d2_d3(),
         },
         "alpha1_alpha3_rho1_rho2_rho3": {
             "D2": deviators_2.dev2_by_alpha1_alpha3(),
             "D4": deviators_4.dev4_ortho_by_rho1_rho2_rho3(),
         },
-        "alpha1_alpha3_a_b_c": {
+        "alpha1_alpha3_d1_d2_d3": {
             "D2": deviators_2.dev2_by_alpha1_alpha3(),
             "D4": deviators_4.dev4_ortho_by_d1_d2_d3(),
         },
     },
     "monoclinic": {
-        "alpha1_alpha3_a_b_c_m_n": {
+        "alpha1_alpha3_d1_d2_d3_d4_d5": {
             "D2": deviators_2.dev2_by_alpha1_alpha3(),
             "D4": deviators_4.dev4_monoclinic_by_d1_d2_d3_d4_d5(),
         },
     },
     "triclinic": {
-        "la_0_la_1_d_0_d_1_d_2_d_3_d_4_d_5_d_6_d_7_d_8": {
-            "D2": deviators_2.dev2_by_la0_la1(),
+        "la1_la2_d1_d2_d3_d4_d5_d6_d7_d8_d9": {
+            "D2": deviators_2.dev2_by_la1_la2(),
             "D4": deviators_4.dev4_triclinic_by_d(),
         },
     },
 }
 
 N4s_parametric = {
-    key: {
-        key_sub: sb.special.combine_to_N4(**val_sub) for key_sub, val_sub in val.items()
-    }
+    key: {key_sub: utils.combine_to_N4(**val_sub) for key_sub, val_sub in val.items()}
     for key, val in N4s_pairs.items()
 }
