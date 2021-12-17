b94802f777d090093f41a5b3b6b5b7560165d7a6
JulianKarlBauer <juliankarlbauer@gmx.de>
2021-12-17 18:14:01 +0100

    Add simple script printing N2s to prompt

---
+++ vofotensors/scripts/print_parameterizations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import sympy as sp
+import vofotensors as vot
+
+
+def print_nested(dictionary):
+    for each in dictionary:
+        if type(dictionary[each]) == dict:
+            print("###")
+            print("Group:", each)
+            print_nested(dictionary[each])
+        else:
+            print(each)
+            print(dictionary[each])
+            print()
+
+
+print("Available parameterizations of second order fiber orientation tensors:\n")
+print_nested(vot.fabric_tensors.N2s_parametric)
