420f78d4adedbbae153476b81d3e767971c8831a
JulianKarlBauer <juliankarlbauer@gmx.de>
2022-02-22 19:35:10 +0100

    Fix isotropic N4

--- vofotensors/basic_tensors.py
+++ vofotensors/basic_tensors.py
@@ -8,7 +8,7 @@ N2_iso = sp.S(1) / sp.S(3) * np.array(sp.eye(3))
 
 
 I2 = sp.eye(3)
-P1 = to_mandel(sp.tensorproduct(I2, I2))
+P1 = to_mandel(sp.tensorproduct(I2, I2)) / sp.S(3)
 P2 = sp.eye(6) - P1
 
 N4_iso = sp.S(1) / sp.S(3) * P1 + sp.S(2) / sp.S(15) * P2
