8daa2832d2d7292c25b03fb9d6de8e780ed81d36
Julian Karl Bauer <juliankarlbauer@gmx.de>
2022-05-05 15:10:27 +0200

    Fix planar substitutions (starting number from 1 had been done wrong)

--- vofotensors/substitutions.py
+++ vofotensors/substitutions.py
@@ -16,6 +16,7 @@ from vofotensors.abc import (
     d6,
     d7,
     d8,
+    d9,
 )
 
 alpha1_in_la1 = sp.sympify("4/3") * la1 - sp.sympify("2/3")
@@ -23,22 +24,22 @@ alpha1_in_la1 = sp.sympify("4/3") * la1 - sp.sympify("2/3")
 substitutions = {
     "planar_alpha_d": {
         alpha3: alpha1 / sp.S(2) - sp.sympify("1/3"),
-        d1: sp.sympify("1/140") * (-sp.S(15) * alpha1 - sp.S(6)),
-        d2: sp.sympify("1/140") * (sp.S(15) * alpha1 - sp.S(6)),
-        d3: z,
+        d2: sp.sympify("1/140") * (-sp.S(15) * alpha1 - sp.S(6)),
+        d3: sp.sympify("1/140") * (sp.S(15) * alpha1 - sp.S(6)),
         d4: z,
         d5: z,
         d6: z,
-        d8: -d7,
+        d7: z,
+        d9: -d8,
     },
     "planar_la1_d": {
         la2: sp.S(1) - la1,
-        d1: sp.sympify("1/140") * (-sp.S(15) * alpha1_in_la1 - sp.S(6)),
-        d2: sp.sympify("1/140") * (sp.S(15) * alpha1_in_la1 - sp.S(6)),
-        d3: z,
+        d2: sp.sympify("1/140") * (-sp.S(15) * alpha1_in_la1 - sp.S(6)),
+        d3: sp.sympify("1/140") * (sp.S(15) * alpha1_in_la1 - sp.S(6)),
         d4: z,
         d5: z,
         d6: z,
-        d8: -d7,
+        d7: z,
+        d9: -d8,
     },
 }
