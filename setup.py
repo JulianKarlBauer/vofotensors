8164d4355b7f31cb727f7135ed0371579e026be5
Julian Karl Bauer <juliankarlbauer@gmx.de>
2022-05-05 14:28:20 +0200

    Add long description from README

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 import setuptools
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
 setuptools.setup(
     name="vofotensors",
@@ -10,6 +15,8 @@ setuptools.setup(
     "Bauer JK, Böhlke T. Variety of fiber orientation tensors. "
     "Mathematics and Mechanics of Solids. December 2021. "
     "doi:10.1177/10812865211057602",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/JulianKarlBauer/fiber_orientation_tensors_2021",
     packages=["vofotensors"],
     package_dir={"vofotensors": "vofotensors"},
