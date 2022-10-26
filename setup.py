7f17a4788eff5cf87b0cd9230e5d4faeb09c8287
Julian Karl Bauer <juliankarlbauer@gmx.de>
2022-10-26 22:06:30 +0200

    Increment version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setuptools.setup(
     name="vofotensors",
-    version="1.0.3",
+    version="1.0.4",
     author="Julian Karl Bauer",
     author_email="juliankarlbauer@gmx.de",
     description="V(ariety)O(f)F(iber)O(rientation)TENSORS "
